1ed9edb43e718ee6c3e19476d6e56dc2fae2c35e
unknown <3203317@qq.com>
2015-12-28 21:41:27 +0800

    haha

--- template/server/app.js
+++ template/server/app.js
@@ -22,6 +22,20 @@ process.on('exit', function (code){
 
 app.createApp(null, function(){
 	var self = this;
+
+	self.configure('production|development', function(){
+		// TODO
+	});
+
+	self.configure('production|development', 'connector', function(){
+		app.set('', {
+			connector : app.connectors.hyxconnector,
+			heartbeat : 3,
+			useDict : true,
+			useProtobuf : true
+		});
+	});
+
 	// TODO
 	self.start(function (err){
 		if(err) return console.error('[ERROR] [%s] app start error: %j.'.red, utils.format(), err.message);
