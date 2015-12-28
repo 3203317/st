44093300909740e3129f26c091bab6c77f2ce389
unknown <3203317@qq.com>
2015-12-28 20:09:29 +0800

    haha

---
+++ template/server/app.js
@@ -0,0 +1,29 @@
+/*!
+ * speedt
+ * Copyright(c) 2015 speedt <3203317@qq.com>
+ * BSD 3 Licensed
+ */
+'use strict';
+
+var utils = require('speedt-utils'),
+    colors = require('colors');
+
+var app = require('../../');
+
+process.on('uncaughtException', function (err){
+	console.error(err.stack.red);
+});
+
+process.on('exit', function (code){
+	if(0 === code) return console.warn('[WARN ] [%s] process exit.'.yellow, utils.format());
+	// TODO
+	console.error('[ERROR] [%s] process exit with code: %s.', utils.format(), code);
+});
+
+app.createApp(null, function(){
+	var self = this;
+	// TODO
+	self.start(function (err){
+		if(err) return console.error('[ERROR] [%s] app start error: %j.'.red, utils.format(), err.message);
+	});
+});
---
+++ template/server/settings.js
@@ -0,0 +1,19 @@
+/*!
+ * speedt
+ * Copyright(c) 2015 speedt <3203317@qq.com>
+ * BSD 3 Licensed
+ */
+'use strict';
+
+module.exports = {
+	mail: {
+		secureConnection: true,
+		host: 'smtp.163.com',
+		port: 465,
+		to: ['huangxin@foreworld.net'],
+		auth: {
+			user: 'firefrog@163.com',
+			pass: ''
+		}
+	}
+};
